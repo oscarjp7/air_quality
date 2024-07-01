56d4ac52e3cd7fdb84af2ff6725c8f26f60e3dd3
Oscar Petersen <oscarpetersen7@gmail.com>
2024-07-01 10:08:46 +0100

    fixed plots

--- DATA/visdata.py
+++ DATA/visdata.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 def plot_year_data(ax, data, boroughs, year_column, cmap, norm, pollutant):
     boroughs.plot(ax=ax, color='white', edgecolor='black')
-    data.plot(column=year_column, ax=ax, legend=True, cmap=cmap, markersize=5, norm=norm)
+    data.plot(column=year_column, ax=ax, legend=False, cmap=cmap, markersize=5, norm=norm)
     ax.set_title(f'{pollutant}(µg/m3) Concentration in London - {year_column.split("_")[0]}')
 
 def plot_pollutant_borough(joined_data, boroughs, pollutant):
@@ -12,10 +12,16 @@ def plot_pollutant_borough(joined_data, boroughs, pollutant):
     cmap = plt.cm.OrRd
     norm = colors.BoundaryNorm(boundaries=np.arange(joined_data[['2013_data', '2016_data', '2019_data']].min().min(),45, 0.5).tolist()
                         + np.arange(50, joined_data[['2013_data', '2016_data', '2019_data']].max().max(), 10).tolist(), ncolors=cmap.N, clip=True)
-    plot_year_data(axs[0], joined_data, boroughs, '2013_data', cmap, norm, pollutant)
-    plot_year_data(axs[1], joined_data, boroughs, '2016_data', cmap, norm, pollutant)
-    plot_year_data(axs[2], joined_data, boroughs, '2019_data', cmap, norm, pollutant)
-    plt.tight_layout()
+    for i, year in enumerate(['2013','2016','2019']):
+        plot_year_data(axs[i], joined_data, boroughs, f'{year}_data', cmap, norm, pollutant)
+
+    cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7])
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    cbar = fig.colorbar(sm, cax=cbar_ax)
+    cbar.set_label(f'{pollutant} Concentration (µg/m³)')
+
+    # plt.tight_layout()
     plt.show()
 
 def plot_year_data_2(ax, data, year_column, cmap, norm, pollutant):
@@ -33,5 +39,6 @@ def plot_pollutant_data(joined_data, pollutant):
     plot_year_data_2(axs[0], joined_data, '2013_data', cmap, norm, pollutant)
     plot_year_data_2(axs[1], joined_data, '2016_data', cmap, norm, pollutant)
     plot_year_data_2(axs[2], joined_data, '2019_data', cmap, norm, pollutant)
+
     plt.tight_layout()
     plt.show()
